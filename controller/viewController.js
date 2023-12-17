2d5bdf7fbef65f3871fd90a69da3dc86d961fc7b
Om Bhandari <om@Kali.om@kali.com>
2023-12-17 13:24:12 +0530

    added search post functionality

--- controller/viewController.js
+++ controller/viewController.js
@@ -1,14 +1,22 @@
 const Posts = require('./../model/imageModel.js');
 const uploadController = require('./../controller/uploadController.js');
 
-exports.index = async(req, res, next)=>{
-    const post = await Posts.find();
+exports.index = async(req, res) => {
+    let post;
+    if(req.query.searchPost){
+        // post = await Posts.find({imageName: req.query.searchPost});
+        post = await Posts.find({imageName: { $regex: req.query.searchPost, $options: 'i'}})
+        //regex and options is used for case insensitive query
+    }
+    else{
+        post = await Posts.find();
+    }
 
     res.status(200).render('index', {
         title: 'Home', 
         allPosts: post
     });
-};
+}
 
 exports.login = async (req, res, next)=>{
     res.status(200).render('login', {
