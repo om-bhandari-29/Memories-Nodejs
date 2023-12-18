d974d8e733af714990afa7562ba2c5206f449ce3
Om Bhandari <om@Kali.om@kali.com>
2023-12-18 09:08:56 +0530

    added post deletion functionality

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -20,9 +20,11 @@ const deleteClick = document.getElementById('postDeleteButton');
 if(deleteClick)
 {
     deleteClick.addEventListener('click', e => {
-        const postid = e.target.dataset.postId;
-        // const postId = e.target.closest(['postId']);
+        const postid = e.target.dataset.postid;
+        // const postid = e.target.dataset.postId;
+
+        //in postDetails.pug, in button post Id is stored in variable called as 'data-post-id', where 'data' is acting like a keyword and it is necessary to use it, and 'post-id' here will be treated as postId,means '-'(dash) will be converted to camel case, or instead of this we can use single word after data but without any capital letters example, 'data-post-id' will be 'postId' and 'data-postid' will be 'postid'
         console.log(postid);
-        // deletePost(postId);
+        deletePost(postid);
     })
 }
